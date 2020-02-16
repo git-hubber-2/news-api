e3d400cca8bfd1ecc0bcd832e927bad1507909dd
Martin S <info@mgdsventures.com>
2020-02-16 21:52:09 +1100

    small app component refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+
 import Articles from './components/Articles';
-
 import Header from './components/Header';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
+
+const App: React.FunctionComponent = (): JSX.Element => {
   return (
     <div className='App'>
       <Container>
@@ -14,6 +14,6 @@ function App() {
       </Container>
     </div>
   );
-}
+};
 
 export default App;
