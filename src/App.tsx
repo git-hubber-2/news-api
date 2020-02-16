6fb6c5496c174311b155ecf3180bf0daecb235f3
Martin S <info@mgdsventures.com>
2020-02-16 22:08:28 +1100

    add inital news-api article fetch - without filtering

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,27 @@ const App: React.FunctionComponent = (): JSX.Element => {
   const [articles, setArticles] = React.useState<articlesArray>([]);
   const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
   const [filter, setFilter] = React.useState<string>('');
-  // hello
+
+  React.useEffect(() => {
+    const apiKey = '96e7efbae84544aca2e40f5834bf2777';
+    const url = `http://newsapi.org/v2/top-headlines?country=au&apiKey=${apiKey}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result.articles); //TODO: remove
+          setArticles(result.articles);
+          setIsLoaded(true);
+        },
+
+        error => {
+          setArticles([]);
+          setIsLoaded(false);
+        }
+      );
+  }, []);
+
   return (
     <div className='App'>
       <Container>
